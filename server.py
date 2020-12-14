3b6e651d2b84f71b1d292cb38798279b48b5b532
kama93 <kamilaszabela@gmail.com>
2020-12-14 20:35:17 +0000

    restored ios folder

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 import pandas as pd
 import numpy as np
 import requests
+from booking_scraper import bkscraper
 from flask import request, jsonify
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt, check_password_hash
@@ -111,44 +112,7 @@ def get_safe_info():
 
 
 # booking.com scraping
-# @app.route('/hotel', methods=['GET'])
-# def scrape(url):
-#     headers = {
-#         'Connection': 'keep-alive',
-#         'Pragma': 'no-cache',
-#         'Cache-Control': 'no-cache',
-#         'DNT': '1',
-#         'Upgrade-Insecure-Requests': '1',
-#         # You may want to change the user agent if you get blocked
-#         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
-#         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-
-#         'Referer': 'https://www.booking.com/index.en-gb.html',
-#         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-#     }
-#     # Download the page using requests
-#     print("Downloading %s"%url)
-#     r = requests.get(url, headers=headers)
-#     # Pass the HTML of the page and create
-#     return e.extract(r.text,base_url=url)
-
-# with open("url.txt",'r') as urllist, open('data.csv','w') as outfile:
-#     fieldnames = [
-#         "name",
-#         "location",
-#         "price",
-#         "price_for",
-#         "room_type",
-#         "beds",
-#         "rating",
-#         "rating_title",
-#         "number_of_ratings",
-#         "url"
-#     ]
-#     writer = csv.DictWriter(outfile, fieldnames=fieldnames,quoting=csv.QUOTE_ALL)
-#     writer.writeheader()
-#     for url in urllist.readlines():
-#         data = scrape(url)
-#         if data:
-#             for h in data['hotels']:
-#                 writer.writerow(h)
+@app.route('/hotel', methods=['GET'])
+def scrape():
+    result = bkscraper.get_result(city="New York", limit=1, detail=False)
+    return jsonify(result)
