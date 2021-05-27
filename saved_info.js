2377e9cfa868d96c56b05d65a1a6779326380012
kama93 <kamilaszabela@gmail.com>
2021-05-27 13:45:13 +0100

    add fetch to save info

--- saved_info.js
+++ saved_info.js
@@ -4,10 +4,30 @@ let width = Dimensions.get('window').width;
 
 const SavedInfo = () => {
     const [country, setCountry] = useState('');
+    const [info, setInfo] = useState(null);
+
+    const lookingForInfo = () => {
+          fetch(`http://127.0.0.1:5000/safe_info/${country}`, {
+            method: 'get',
+            headers: { 'Content-Type': 'application/json' }
+          })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.health.diseasesAndVaccinesInfo.Food/Water.description)
+                setInfo(data)
+            })
+      }
+
+    const clean = () =>{
+        setInfo(null)
+        setCountry('')
+    }
+
     return (
         <View>
             <ImageBackground source={require('./image/passport.png')} resizeMode='cover' style={styles.image} imageStyle={{ opacity: 0.2 }}>
-                <View>
+                {!info?
+                (<View>
                     <View>
                         <TouchableOpacity style={styles.inputStyle}>
                             <TextInput style={styles.itemText} placeholder='Country' placeholderTextColor='#505050' onChangeText={setCountry}>
@@ -15,11 +35,24 @@ const SavedInfo = () => {
                         </TouchableOpacity>
                     </View>
                     <View style={{ marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
-                        <TouchableOpacity style={{ backgroundColor: '#3D6DCC', width: 270, borderRadius: 7, marginRight: 10 }} onPress={() => lookingForHotel()}>
+                        <TouchableOpacity style={{ backgroundColor: '#3D6DCC', width: 270, borderRadius: 7, marginRight: 10 }} onPress={() => lookingForInfo()}>
                             <Text style={{ color: 'white', textAlign: 'center', padding: 10, fontFamily: 'Architects Daughter Regular' }}>Check</Text>
                         </TouchableOpacity>
                     </View>
-                </View>
+                </View>):(
+                    <View>
+                        <View style={{ marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
+                        <TouchableOpacity style={{ backgroundColor: '#3DCC6D', width: 270, borderRadius: 7, marginRight: 10 }} onPress={() => clean()}>
+                            <Text style={{ color: 'white', textAlign: 'center', padding: 10, fontFamily: 'Architects Daughter Regular' }}>Clean</Text>
+                        </TouchableOpacity>
+                        <View>
+                           <Text>{info.advisoryText}</Text>
+                           <Text>Food and water</Text>
+                           {/* <Text>{info.health.diseasesAndVaccinesInfo}</Text> */}
+                        </View>
+                    </View>
+                    </View>
+                )}
             </ImageBackground>
         </View>
     )
